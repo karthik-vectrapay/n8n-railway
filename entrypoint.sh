2d2044984353cc606cf1a30a9606c3fbfa3aadd8
karthik-vectrapay <karthik@vectra-pay.com>
2026-04-07 13:31:11 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 # Fix permissions at runtime AFTER volume is mounted
 mkdir -p /home/node/.n8n
-chown -R node:node /home/node/.n8n
-chmod -R 755 /home/node/.n8n
+chmod -R 777 /home/node/.n8n
 
-# Run original entrypoint as node user
-exec su-exec node tini -- /docker-entrypoint.sh
+# Run as current user
+exec tini -- /docker-entrypoint.sh
