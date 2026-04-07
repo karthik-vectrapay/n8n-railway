639dc58b62c0522a27718ead9fb43b32750633a6
karthik-vectrapay <karthik@vectra-pay.com>
2026-04-07 13:18:58 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ mkdir -p /home/node/.n8n
 chown -R node:node /home/node/.n8n
 chmod -R 755 /home/node/.n8n
 
-# Switch to node user and start n8n
-exec su-exec node tini -- /docker-entrypoint.sh
+# Run original entrypoint as node user
+exec gosu node tini -- /docker-entrypoint.sh
